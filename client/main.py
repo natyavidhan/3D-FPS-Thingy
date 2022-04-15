0cb4e31fe7ee811154565364b2792ee8920b931e
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-04-16 01:54:22 +0530

    multiplayer

--- client/main.py
+++ client/main.py
@@ -3,11 +3,34 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 import pyautogui
 import random
 import math
+import socket
 import json
+
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = "localhost"
+        self.port = 5555
+        self.addr = (self.host, self.port)
+        self.connect()
+
+    def connect(self):
+        self.client.connect(self.addr)
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            response = self.client.recv(2048).decode()
+            return response
+        except socket.error as e:
+            return str(e)
+
+net = Network()
 
 app = Ursina()
 Sky()
-window.title = 'FPS'
+window.title = '3D FPS Thingy'
 window.fps_counter.enabled = True
 window.borderless = False
 
@@ -22,24 +45,50 @@ floorcube = Entity(model="cube", color = color.lime, scale=(100, 1, 100), collid
 
 playerController = FirstPersonController()
 playerController.speed = 10
-playerController.position = (0, 100, 0)
+playerController.position = (0, 35, 0)
 
-player = Entity(model="cube", scale=(0.5, 1, 0.5), texture="white_cube", texture_scale=(100, 100),
+players = {}
+playerChar = Entity(model="cube", scale=(0.5, 1, 0.5), texture="white_cube", texture_scale=(100, 100),
                 position=playerController.position, rotation=playerController.rotation)
 
 def update():
     global latest_mouse_pos
-    global player
+    global players, playerChar
     if held_keys['f']:
         camera.fov += 1
     if held_keys['r']:
         camera.fov -= 1
     if playerController.y < 0:
-        playerController.y = 0
+        playerController.y = 35
     
+    pos = []
+    rot = []
+    for i in range(3):
+        pos.append(round(list(playerController.position)[i], 4))
+        rot.append(round(list(playerController.rotation)[i], 4))
+
+    player = {"pos": pos, "rot": rot}
+    net.send("update||" + json.dumps(player))
+
+    allPlayers = json.loads(net.send("get||all"))
+    user = json.loads(net.send("get||self"))
+    for addr in allPlayers:
+        if addr != user["id"]:
+            player = allPlayers[addr]
+            pos = Vec3(player["pos"][0], player["pos"][1], player["pos"][2])
+            rot = Vec3(player["rot"][0], player["rot"][1], player["rot"][2])
+
+            if addr not in players:
+                players[addr] = Entity(model="cube", color = color.red, scale=(1, 1, 1), collider="box", position=pos, rotation=rot)
+            else:
+                players[addr].position = pos
+                players[addr].rotation = rot
+
+    
+
     x, y, z = playerController.position
     rotX, rotY, rotZ = playerController.rotation
-    player.position = Vec3(float(x), float(y)+0.5, float(z))
-    player.rotation = Vec3(float(rotX), float(rotY), float(rotZ))
+    playerChar.position = Vec3(float(x), float(y)+0.5, float(z))
+    playerChar.rotation = Vec3(float(rotX), float(rotY), float(rotZ))
 
 app.run()
---
+++ server/main.py
@@ -0,0 +1,64 @@
+import socket
+from _thread import *
+from threading import Thread
+import sys
+import random
+import json
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = "localhost"
+port = 5555
+server_ip = socket.gethostbyname(server)
+try:
+    s.bind((server, port))
+except socket.error as e:
+    print(str(e))
+    
+s.listen(10)
+print("Waiting for a connection")
+
+players = {}
+mapSize = [100, 100]
+
+
+def threaded_client(conn, addr):
+    global players
+    players[str(addr)] = {"id": str(addr), "pos": [0, 10, 0], "rot": [0, 0, 0]}
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+            if not data:
+                conn.send(str.encode("Goodbye"))
+                break
+            message, command = reply.split("||")
+            if message == "get":
+                if command == "all":
+                    conn.send(str.encode(json.dumps(players)))
+                elif command == "self":
+                    conn.send(str.encode(json.dumps(players[str(addr)])))
+            elif message == "update":
+                player = json.loads(command)
+                player["id"] = str(addr)
+                players[str(addr)] = player
+                conn.send(str.encode("updated"))
+
+        except Exception as e:
+            traceback = sys.exc_info()[2].tb_lineno
+            print(traceback)
+            break
+    print("Connection Closed")
+    conn.close()
+    players.pop(str(addr))
+
+
+def server():
+    while True:
+        conn, addr = s.accept()
+        print("Connected to: ", addr)
+        start_new_thread(threaded_client, (conn, addr))
+
+start_new_thread(server, ())
+while True:
+    pass
